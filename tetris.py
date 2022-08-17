34d8209101be9cb76d2e38ff5524b89bb3a4c263
Jacob <23357015+jbakermans@users.noreply.github.com>
2022-08-17 18:23:27 -0400

    Create tetris DSL for REPL
    Use the REPL API to set up the DSL for tetris

---
+++ tetris.py
@@ -0,0 +1,379 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 16 22:22:13 2022
+
+@author: jbakermans
+"""
+
+import pickle
+import numpy as np
+
+from API import *
+
+from randomSolver import *
+from pointerNetwork import *
+from programGraph import *
+from SMC import *
+from ForwardSample import *
+from MCTS import MCTS
+from CNN import *
+
+import dsl
+
+import time
+import random
+from scipy.signal import convolve2d
+
+RESOLUTION = 16
+
+import torch
+import torch.nn as nn
+
+class Tetris(Program):
+    lexicon = ['hor', 'vert'] \
+        + ['s' + str(i) for i in range(9)] \
+            + [i - int(RESOLUTION / 2) for i in range(RESOLUTION)]
+
+    def __init__(self):
+        self._rendering = None
+
+    def __repr__(self):
+        return str(self)
+
+    def __ne__(self, o): return not (self == o)
+
+    def execute(self):
+        # Only render if this has not been rendered before
+        if self._rendering is None: self._rendering = self.render()
+        return self._rendering
+
+    def IoU(self, other):
+        # Calculate 'area under curve' between self and other object
+        if isinstance(other, Tetris): other = other.execute()
+        # Find maximum overlap between self and other through 2d convolution
+        overlap = np.max(np.convolve2d(self.execute(), other))
+        # Find maximum overshoot from number of pixels in self more than other
+        overshoot = max([0, np.sum(self.execute()) - np.sum(other)])
+        # Find the match between self and other. Note this isn't symmetric:
+        # being smaller than other is OK, but being larger is punished
+        return (overlap - overshoot) / (overlap + overshoot)
+    
+    def render(self):
+        # Render function will depend on operation; return None by default
+        return None
+
+# The type of CSG's
+tTetris = BaseType(Tetris)
+
+class Primitive(Tetris):
+    token = 's'    
+    type = arrow(tTetris, tTetris)
+    
+    def __init__(self, index=0):
+        super().__init__()        
+        self.shape = dsl.PRIMITIVES[index]
+        self.index = index
+        self.token += str(index)
+        
+    def toTrace(self): return [self]
+
+    def __str__(self):
+        return self.token
+
+    def children(self): return []
+
+    def __eq__(self, o):
+        return isinstance(o, Primitive) and o.index == self.index
+
+    def __hash__(self):
+        return hash((self.token))
+    
+    def __contains__(self, p):
+        # Use contains to find if queried program occurs anywhere in full program
+        return p == self
+
+    def serialize(self):
+        return self.token
+    
+    def render(self):
+        return self.shape
+    
+class Hor(Tetris):
+    token = 'hor'
+    type = arrow(integer(-int(RESOLUTION / 2), RESOLUTION - 1 - int(RESOLUTION / 2)), 
+                 tTetris, tTetris, tTetris)
+    
+    def __init__(self, a, b, shift=0):
+        super(Hor, self).__init__()
+        self.elements = [a,b]
+        self.shift = shift
+
+    def toTrace(self):
+        return self.elements[0].toTrace() + self.elements[1].toTrace() + [self]
+
+    def __str__(self):
+        return f"hor({str(self.elements[0])}, {str(self.elements[1])}; {str(self.shift)})"
+
+    def children(self): return self.elements
+
+    def serialize(self):
+        return ('hor',list(self.elements)[0],list(self.elements)[1], self.shift)
+
+    def __eq__(self, o):
+        return isinstance(o, Hor) and tuple(o.elements) == tuple(self.elements) \
+            and o.shift == self.shift
+
+    def __contains__(self, p):
+        # Use contains to find if queried program occurs anywhere in full program
+        if p == self: 
+            return True
+        else: 
+            return any(p in e for e in self.elements)
+
+    def __hash__(self):
+        return hash((self.token, tuple(self.elements)))
+    
+    def render(self):
+        return dsl.hor(self.elements[0].render(), 
+                       self.elements[1].render(), 
+                       shift=self.shift)
+    
+    
+class Vert(Tetris):
+    token = 'vert'
+    type = arrow(integer(-int(RESOLUTION / 2), RESOLUTION - 1 - int(RESOLUTION / 2)), 
+                 tTetris, tTetris, tTetris)
+    
+    def __init__(self, a, b, shift=0):
+        super(Vert, self).__init__()
+        self.elements = [a,b]
+        self.shift = shift
+
+    def toTrace(self):
+        return self.elements[0].toTrace() + self.elements[1].toTrace() + [self]
+
+    def __str__(self):
+        return f"vert({str(self.elements[0])}, {str(self.elements[1])}; {str(self.shift)})"
+
+    def children(self): return self.elements
+
+    def serialize(self):
+        return ('vert',list(self.elements)[0],list(self.elements)[1], self.shift)
+
+    def __eq__(self, o):
+        return isinstance(o, Vert) and tuple(o.elements) == tuple(self.elements) \
+            and o.shift == self.shift
+            
+    def __contains__(self, p):
+        # Use contains to find if queried program occurs anywhere in full program
+        if p == self: 
+            return True
+        else: 
+            return any(p in e for e in self.elements)            
+
+    def __hash__(self):
+        return hash((self.token, tuple(self.elements)))
+    
+    def render(self):
+        return dsl.vert(self.elements[0].render(), 
+                        self.elements[1].render(), 
+                        shift=self.shift)    
+    
+tDSL = DSL([Hor, Vert, Primitive],
+          lexicon=Tetris.lexicon)
+
+"""Neural networks"""
+class ObjectEncoder(CNN):
+    def __init__(self):
+        super(ObjectEncoder, self).__init__(channels=2,
+                                            inputImageDimension=RESOLUTION)
+
+    def forward(self, spec, obj):
+        if isinstance(obj, list): # batched - expect a single spec and multiple objects
+            spec = np.repeat(spec[np.newaxis,:,:],len(obj),axis=0)
+            obj = np.stack(obj)
+            return super(ObjectEncoder, self).forward(np.stack([spec, obj],1))
+        else: # not batched
+            return super(ObjectEncoder, self).forward(np.stack([spec, obj]))
+        
+
+class SpecEncoder(CNN):
+    def __init__(self):
+        super(SpecEncoder, self).__init__(channels=1,
+                                          inputImageDimension=RESOLUTION)
+
+
+"""Training"""
+def randomScene(maxShapes=5, minShapes=1, verbose=False, export=None, no_repeat=False):
+    # Choose number of shapes to include
+    desiredShapes = np.random.randint(minShapes, maxShapes + 1)
+    # Generate initial shape
+    s=Primitive(np.random.randint(len(dsl.PRIMITIVES)))
+    for _ in range(desiredShapes - 1):
+        # Sample new primitive and add to list of arguments
+        o = [s, Primitive(np.random.randint(len(dsl.PRIMITIVES)))]
+        # Resample until new primitive until it's unique, if required
+        if no_repeat:
+            while o[1] in o[0]:
+                o[1] = Primitive(np.random.randint(len(dsl.PRIMITIVES)))
+        # Shuffle objects randomly, so which goes where is random
+        np.random.shuffle(o)
+        # Get shapes of rendered version of both, because that will constrain shift
+        d = [i.execute().shape for i in o]        
+        # Randomly choose to      
+        if np.random.rand() > 0.5:
+            # Horizontal concetanation, with random shift depending on heights
+            s = Hor(o[0], o[1], shift=np.random.randint(-d[1][0] + 1, d[0][0]))
+        else:
+            # Vertical concetanation, with random shift depending on widths
+            s = Vert(o[0], o[1], shift=np.random.randint(-d[1][1] + 1, d[0][1]))
+    if verbose:
+        print(s)
+        print(ProgramGraph.fromRoot(s, oneParent=True).prettyPrint())
+        import matplotlib.pyplot as plt
+        plt.imshow(s.execute(), cmap='Greys', vmin=0, vmax=1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(s)
+        plt.show()
+    if export:
+        import matplotlib.pyplot as plt        
+        plt.imshow(s.execute(), cmap='Greys', vmin=0, vmax=1)
+        plt.xticks([])
+        plt.yticks([])        
+        plt.title(s)        
+        plt.savefig(export)
+        plt.show()        
+    return s
+
+def trainCSG(m, getProgram, trainTime=None, checkpoint=None):
+    print("cuda?",m.use_cuda)
+    assert checkpoint is not None, "must provide a checkpoint path to export to"
+    
+    optimizer = torch.optim.Adam(m.parameters(), lr=0.001, eps=1e-3, amsgrad=True)
+    
+    startTime = time.time()
+    reportingFrequency = 100
+    totalLosses = []
+    movedLosses = []
+    iteration = 0
+
+    while trainTime is None or time.time() - startTime < trainTime:
+        s = getProgram()
+        l = m.gradientStepTrace(optimizer, s.execute(), s.toTrace())
+        totalLosses.append(sum(l))
+        movedLosses.append(sum(l)/len(l))
+
+        if iteration%reportingFrequency == 0:
+            print(f"\n\nAfter {iteration} gradient steps...\n\tTrace loss {sum(totalLosses)/len(totalLosses)}\t\tMove loss {sum(movedLosses)/len(movedLosses)}\n{iteration/(time.time() - startTime)} grad steps/sec")
+            totalLosses = []
+            movedLosses = []
+            with open(checkpoint,"wb") as handle:
+                pickle.dump(m, handle)
+
+        iteration += 1
+
+def testCSG(m, getProgram, timeout, export):
+    oneParent = m.oneParent
+    solvers = [# RandomSolver(dsl),
+               # MCTS(m, reward=lambda l: 1. - l),
+               # SMC(m),
+               ForwardSample(m, maximumLength=18)]
+    loss = lambda spec, program: 1-max( o.IoU(spec) for o in program.objects() ) if len(program) > 0 else 1.
+
+    testResults = [[] for _ in solvers]
+
+    for _ in range(30):
+        spec = getProgram()
+        print("Trying to explain the program:")
+        print(ProgramGraph.fromRoot(spec, oneParent=oneParent).prettyPrint())
+        print()
+        for n, solver in enumerate(solvers):
+            testSequence = solver.infer(spec.execute(), loss, timeout)
+            testResults[n].append(testSequence)
+            for result in testSequence:
+                print(f"After time {result.time}, achieved loss {result.loss} w/")
+                print(result.program.prettyPrint())
+                print()
+
+    plotTestResults(testResults, timeout,
+                    defaultLoss=1.,
+                    names=[# "MCTS","SMC", 
+                           "FS"],
+                    export=export)
+
+def plotTestResults(testResults, timeout, defaultLoss=None,
+                    names=None, export=None):
+    import matplotlib.pyplot as plot
+
+    def averageLoss(n, T):
+        results = testResults[n] # list of list of results, one for each test case
+        # Filter out results that occurred after time T
+        results = [ [r for r in rs if r.time <= T]
+                    for rs in results ]
+        losses = [ min([defaultLoss] + [r.loss for r in rs]) for rs in results ]
+        return sum(losses)/len(losses)
+
+    plot.figure()
+    plot.xlabel('Time')
+    plot.ylabel('Average Loss')
+
+    for n in range(len(testResults)):
+        xs = list(np.arange(0,timeout,0.1))
+        plot.plot(xs, [averageLoss(n,x) for x in xs],
+                  label=names[n])
+    plot.legend()
+    if export:
+        plot.savefig(export)
+    else:
+        plot.show()
+        
+        
+    
+        
+    
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description = "")
+    parser.add_argument("mode", choices=["train","test","demo"])
+    parser.add_argument("--checkpoint", default="checkpoints/CSG.pickle")
+    parser.add_argument("--maxShapes", default=2,
+                            type=int)
+    parser.add_argument("--trainTime", default=None, type=float,
+                        help="Time in hours to train the network")
+    parser.add_argument("--attention", default=1, type=int,
+                        help="Number of rounds of self attention to perform upon objects in scope")
+    parser.add_argument("--heads", default=2, type=int,
+                        help="Number of attention heads")
+    parser.add_argument("--hidden", "-H", type=int, default=256,
+                        help="Size of hidden layers")
+    parser.add_argument("--timeout", default=5, type=float,
+                        help="Test time maximum timeout")
+    parser.add_argument("--oneParent", default=False, action='store_true')
+    arguments = parser.parse_args()
+
+    if arguments.mode == "demo":
+        for n in range(100):
+            randomScene(export=f"/tmp/CAD_{n}.png",maxShapes=arguments.maxShapes)
+        import sys
+        sys.exit(0)
+        
+            
+
+    if arguments.mode == "train":
+        m = ProgramPointerNetwork(ObjectEncoder(), SpecEncoder(), dsl,
+                                  oneParent=arguments.oneParent,
+                                  attentionRounds=arguments.attention,
+                                  heads=arguments.heads,
+                                  H=arguments.hidden)
+        trainCSG(m, lambda: randomScene(maxShapes=arguments.maxShapes),
+                 trainTime=arguments.trainTime*60*60 if arguments.trainTime else None,
+                 checkpoint=arguments.checkpoint)
+    elif arguments.mode == "test":
+        with open(arguments.checkpoint,"rb") as handle:
+            m = pickle.load(handle)
+        testCSG(m,
+                lambda: randomScene(maxShapes=arguments.maxShapes, minShapes=arguments.maxShapes), arguments.timeout,
+                export=f"figures/CAD_{arguments.maxShapes}_shapes.png")
